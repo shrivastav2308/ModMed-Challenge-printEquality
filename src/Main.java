e62361c34b908211929c5d07667d17fb34dff59e
Piyush Srivastava <piyush.srivastava@office365.3pillarglobal.com>
2022-11-05 13:25:35 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        printEquality(1,1,1);
+        printEquality(1,1,2);
+        printEquality(-1,-1,-1);
+    }
+    public static void printEquality(int a, int b, int c){
+        if(a<0 || b<0 || c<0) {
+            System.out.println("Invalid Value");
+        }
+        else if (a==b && b==c){
+            System.out.println("All numbers are equal");
+        }
+        else if (a!=b && b!=c && a!=c){
+            System.out.println("All numbers are different");
+        }
+        else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+}
